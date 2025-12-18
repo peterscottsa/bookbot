96e061900e65f55cf78690c4db210afbd7ef1e6a
Peter Scott <peterscott.sa@gmail.com>
2025-12-18 09:26:51 +0100

    Add arguments support

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ def get_book_text(filepath):
     return file.read()
 
 def main():
+
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  
+  filepath = sys.argv[1]
+  
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
-  book_text = get_book_text('books/frankenstein.txt')
+  print(f"Analyzing book found at {filepath}...")
+  book_text = get_book_text(filepath)
   print("----------- Word Count ----------")
   num_words = get_word_count(book_text)
   print(f"Found {num_words} total words")
